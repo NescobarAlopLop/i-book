44b05fb86f34ea1e39b6633c592fb70cce2c76ed
GeorgeKesaev <kesaev@gmail.com>
2018-11-24 19:33:27 +0200

    fs package

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const PORT = process.env.PORT || 8000;
 let express = require('express');
 let bodyParser = require('body-parser');
 let logRequest = require('log-request');
+let fs = require('fs');
 
 let i_book_router = express.Router();
 i_book_router.get('/:name', findIpByName);
-i_book_router.post('/', storeIP);
+i_book_router.post('/store', storeIP);
 
 let app = express();        // https://expressjs.com/en/guide/routing.html
 app.use(bodyParser.json({ type: 'application/json' }))
@@ -19,6 +20,20 @@ app.use(bodyParser.json({ type: 'application/json' }))
     .use('/', express.static(path.join(__dirname, 'public')))
     .get('/get_all', getAllIps);
 
+// load credentials from file for local use or process.env if on heroku
+if (fs.existsSync('./credentials.json')) {
+    var credentials = JSON.parse(fs.readFileSync('./credentials.json', 'utf8'));
+}
+else {
+    var credentials = process.env;
+}
+
+const mongo_uri = 'mongodb+srv://' + credentials.mongoUser + ':' +
+    credentials.mongoPass + '@' +
+    credentials.mongoServer + '/' +
+    credentials.mongoDB
+    + "?retryWrites=true";
+
 function findIpByName(req, res) {
     console.log('looking for IP');
     return res.json({
@@ -27,9 +42,45 @@ function findIpByName(req, res) {
 }
 
 function storeIP(req, res) {
-    console.log('stroring ip');
-    return res.json({
-        message: 'stroring ip'
+    console.log('Storing ip');
+    MongoClient.connect(mongo_uri, { useNewUrlParser: true }, function (err, db) {
+        // assert.equal(null, err);
+        if (err) {
+            console.error(err);
+            res.statusCode = 500;
+            return res.json({
+                errors: ['Couldnt connect to server']
+            });
+        }
+        console.log("Connected correctly to Mongo server");
+        const dbo = db.db(credentials.mongoDB);
+        let address = req.body;
+        // if ('notes' in address) {
+            // if (address.notes.length <= max_notes) {
+        const collection = dbo.collection(credentials.mongoCollection);
+                collection.insertOne(address, function (err, result) {
+                    if (err) {
+                        console.error(err);
+                        res.statusCode = 500;
+                        return res.json({
+                            errors: ['Failed to save a song']
+                        })
+                    }
+                    res.setHeader('Content-Type', 'application/json');
+                    res.statusCode = 201;
+                    let saved_song_id = result.ops[0]._id;
+                    console.log("one song inserted into db, song id: " + saved_song_id);
+                    res.json(saved_song_id);
+                    db.close();
+                });
+            // }
+            // else {
+                // badSaveObject(res, 'Notes array is limited to ' + max_notes + ' notes');
+            // }
+    //     }
+    //     else {
+    //         badSaveObject(res, 'Wrong object.');
+    //     }
     });
 }
 
