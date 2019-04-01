fa2f7e4b7d3b51aea577033235762576278dd654
George K <kesaev@gmail.com>
2019-04-01 18:57:53 +0300

    requirements

--- client/client.py
+++ client/client.py
@@ -1,8 +1,8 @@
-import json
+# import json
 import datetime
 import time
 import socket
-import pyrebase as firebase
+# import pyrebase as firebase
 
 import firebase_admin
 from firebase_admin import credentials
@@ -16,23 +16,26 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 while True:
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    print(socket.gethostname())
-
-    s.connect(("8.8.8.8", 80))
-    ip = s.getsockname()[0]
-    print(ip)
-    s.close()
-
-    hostname = socket.gethostname()
-    last_updated = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-
-    data = {'ip': ip, 'hostname': hostname, 'last_updated': last_updated}
-
     try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        print(socket.gethostname())
+
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+        print(ip)
+        s.close()
+
+        hostname = socket.gethostname()
+        last_updated = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+
+        data = {'ip': ip, 'hostname': hostname, 'last_updated': last_updated}
+
+
         doc_ref = db.collection(u'address_book').document(hostname)
         doc_ref.set(data)
+    except KeyboardInterrupt:
+        print("Exit on keyboard interrupt.")
     except Exception as e:
         print(e)
-    # time.sleep(3 * 60)
-    time.sleep(3)
+    time.sleep(3 * 60)
+    # time.sleep(3)
